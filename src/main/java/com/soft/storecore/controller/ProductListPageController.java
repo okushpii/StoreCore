f4fdac6a78c1107a065fde1b669952db0e88766b
Eugene Kushpii <jeffhardy2209@gmail.com>
2019-08-26 12:07:37 +0300

    Added categories to product page

--- src/main/java/com/soft/storecore/controller/ProductListPageController.java
+++ src/main/java/com/soft/storecore/controller/ProductListPageController.java
@@ -1,6 +1,7 @@
 package com.soft.storecore.controller;
 
 
+import com.soft.storecore.facade.CategoryFacade;
 import com.soft.storecore.facade.ProductFacade;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,13 +17,16 @@ import static com.soft.storecore.controller.ControllerConstants.*;
 @RequestMapping(RequestMappings.PRODUCT_LIST)
 public class ProductListPageController {
 
-
     @Resource
     private ProductFacade productFacade;
+
+    @Resource
+    private CategoryFacade categoryFacade;
 
     @GetMapping("{id}")
     public String getProductsPage(@PathVariable  Long id, Model model) {
         model.addAttribute(Attributes.PRODUCT_LIST, productFacade.findAllByCategoryId(id));
+        model.addAttribute(Attributes.CATEGORIES, categoryFacade.findAll());
         return Pages.PRODUCT_LIST_PAGE;
     }
 
