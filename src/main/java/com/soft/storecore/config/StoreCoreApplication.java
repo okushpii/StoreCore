1c64dafae2f1cbbbfc2aded89786432f7ad1743a
Eugene Kushpii <jeffhardy2209@gmail.com>
2019-09-21 13:19:37 +0300

    Fixed invalid location for enities

--- src/main/java/com/soft/storecore/config/StoreCoreApplication.java
+++ src/main/java/com/soft/storecore/config/StoreCoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = "com.soft.storecore")
-@EntityScan(value = "com.soft.config.util.entity")
+@EntityScan(value = "com.soft.storecore.core")
 public class StoreCoreApplication {
 
 	public static void main(String[] args) {
