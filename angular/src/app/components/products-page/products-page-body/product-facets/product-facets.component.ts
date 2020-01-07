e4f6e3c03d0ce6f8ee69e9cd83ed8196af0ef801
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-01-07 13:00:08 +0200

    Product page created

---
+++ angular/src/app/components/products-page/products-page-body/product-facets/product-facets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-facets',
+  templateUrl: './product-facets.component.html',
+  styleUrls: ['./product-facets.component.css']
+})
+export class ProductFacetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
