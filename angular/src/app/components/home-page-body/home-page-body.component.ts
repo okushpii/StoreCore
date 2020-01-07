5c1d221a9a5efbe5352d542ac909a8f833580ea0
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-01-07 12:00:45 +0200

    Home page implemented

---
+++ angular/src/app/components/home-page-body/home-page-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-body',
+  templateUrl: './home-page-body.component.html',
+  styleUrls: ['./home-page-body.component.css']
+})
+export class HomePageBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
