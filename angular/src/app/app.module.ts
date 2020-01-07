947c3a1c952943c24c0e7115348bc65332f434e8
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-01-07 12:00:45 +0200

    Home page implemented

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import  {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import { HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {TopBarComponent} from './components/top-bar/top-bar.component';
 import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 import {CategoryDropdownMenuComponent} from './components/category-dropdown-menu/category-dropdown-menu.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageBodyComponent } from './components/home-page-body/home-page-body.component';
 
+
+const routes = [
+  {path: "", component: HomePageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,14 +27,19 @@ import {CategoryDropdownMenuComponent} from './components/category-dropdown-menu
     TopBarComponent,
     NavBarComponent,
     CategoryDropdownMenuComponent,
+    HomePageComponent,
+    FooterComponent,
+    HomePageBodyComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
