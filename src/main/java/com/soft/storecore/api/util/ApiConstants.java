98b728ec716dd15b731e4a13c1203af7cd29742b
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-02-02 13:09:08 +0200

    Cross origin requests refactored

---
+++ src/main/java/com/soft/storecore/api/util/ApiConstants.java
@@ -0,0 +1,8 @@
+package com.soft.storecore.api.util;
+
+public interface ApiConstants {
+
+    String HOST = "localhost";
+    String FE_PORT = "4200";
+    String FE_HTTP_URI = "http://" + HOST + ":" + FE_PORT;
+}
