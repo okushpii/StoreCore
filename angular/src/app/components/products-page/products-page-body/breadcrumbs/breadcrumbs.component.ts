78a842b25684b5d30054986fbc31c5d11b6b18da
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-02-22 10:57:41 +0200

    [FE] Breadcrumbs

--- angular/src/app/components/products-page/products-page-body/breadcrumbs/breadcrumbs.component.ts
+++ angular/src/app/components/products-page/products-page-body/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Category} from "../../../../models/category";
+import {ActivatedRoute} from "@angular/router";
+import {CategoryService} from "../../../../services/category.service";
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  category: Category;
+
+  constructor(
+    private route: ActivatedRoute,
+    private categoryService: CategoryService
+  ) {
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(p => {
+      let categoryCode = p.get("categoryId");
+        this.getCategory(categoryCode);
+    });
+  }
+
+  getCategory(code: string) {
+    this.categoryService.find(code).subscribe(c => {
+      this.category = c;
+    });
   }
 
 }
