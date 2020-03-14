3d5576fda49de509d8546a32a6be0631b110de27
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-03-14 21:19:30 +0200

    [BE] Sorting improvements

--- src/main/java/com/soft/storecore/facade/sorting/data/SortingData.java
+++ src/main/java/com/soft/storecore/facade/sorting/data/SortingData.java
@@ -4,6 +4,7 @@ public class SortingData {
 
     private String code;
     private String name;
+    private boolean byDefault;
 
     public String getCode() {
         return code;
@@ -19,5 +20,13 @@ public class SortingData {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isByDefault() {
+        return byDefault;
+    }
+
+    public void setByDefault(boolean byDefault) {
+        this.byDefault = byDefault;
     }
 }
