9f4da1aa9b07b113c36c92aebc8b2c9f05aec5fa
Eugene Kushpii <jeffhardy2209@gmail.com>
2020-03-14 21:29:27 +0200

    [FE] Sorting implemented

--- angular/src/app/components/products-page/products-page-body/product-sorting/product-sorting.component.ts
+++ angular/src/app/components/products-page/products-page-body/product-sorting/product-sorting.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SortingService} from "../../../../services/sorting.service";
+import {Sorting} from "../../../../models/sorting";
+import {ActivatedRoute} from "@angular/router";
+
+const sortingGroup = "pdp";
+const sortingParam = "sorting";
 
 @Component({
   selector: 'app-product-sorting',
   templateUrl: './product-sorting.component.html',
   styleUrls: ['./product-sorting.component.css']
 })
+
+
 export class ProductSortingComponent implements OnInit {
 
-  constructor() { }
+  sortingList: Sorting[];
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private sortingService: SortingService
+  ) {
   }
 
+  ngOnInit() {
+    this.loadSorting();
+  }
+
+  loadSorting() {
+    this.sortingService.find(sortingGroup).subscribe(s => {
+      this.sortingList = s;
+    })
+  }
+
+  getSelectedSortingName(sortingList: Sorting[]): string {
+    return this.sortingService.getSelectedSortingName(sortingList, sortingParam);
+  }
+
+  sortingIsSelected(sorting: Sorting): boolean {
+    return this.sortingService.sortingIsSelected(sorting, sortingParam);
+  }
 }
