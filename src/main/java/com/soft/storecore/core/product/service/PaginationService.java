f670778001a312b191fc8e1d8b0fdeea92f68ff0
Mixan Panama <mixanpanama@gmail.com>
2020-04-08 13:50:41 +0300

    Count of quantity of pages was implemented

--- src/main/java/com/soft/storecore/core/product/service/PaginationService.java
+++ src/main/java/com/soft/storecore/core/product/service/PaginationService.java
@@ -3,4 +3,7 @@ package com.soft.storecore.core.product.service;
 public interface PaginationService {
 
     int getStart(int pageNumber, int pageSize);
+
+    int getPagesCount(int itemsCount, int pageSize);
 }
+
--- src/test/java/com/soft/storecore/core/product/service/DefaultPaginationServiceTest.java
+++ src/test/java/com/soft/storecore/core/product/service/DefaultPaginationServiceTest.java
@@ -6,7 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class DefaultPaginationServiceTest {
 
+    private static final int ITEMS_COUNT_EVEN = 50;
+    private static final int ITEMS_COUNT_ODD = 35;
     private static final int PAGE_SIZE = 10;
+    private static final int PAGES_COUNT_EVEN = 5;
+    private static final int PAGES_COUNT_ODD = 4;
 
     private DefaultPaginationService testedEntry = new DefaultPaginationService();
 
@@ -22,6 +26,20 @@ public class DefaultPaginationServiceTest {
         int result = testedEntry.getStart(2, PAGE_SIZE);
 
         assertEquals(10, result);
+    }
+
+    @Test
+    public void shouldGetPagesCountIfItemsCountIsEven(){
+        int result = testedEntry.getPagesCount(ITEMS_COUNT_EVEN, PAGE_SIZE);
+
+        assertEquals(PAGES_COUNT_EVEN, result);
+    }
+
+    @Test
+    public void shouldGetPagesCountIfItemsCountIsOdd(){
+        int result = testedEntry.getPagesCount(ITEMS_COUNT_ODD, PAGE_SIZE);
+
+        assertEquals(PAGES_COUNT_ODD, result);
     }
 
 }
